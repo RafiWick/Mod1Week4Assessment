c43b64ed48df7ceaf57e39811d6f89eba0434670
RafiWick <rafiwick@gmail.com>
2023-04-13 09:41:30 -0600

    Rafi's assessment done

--- Vehicle.UnitTests/VehicleTests.cs
+++ Vehicle.UnitTests/VehicleTests.cs
@@ -18,11 +18,32 @@ namespace Vehicle.UnitTests
         {
             Vehicle andysBike = new Vehicle(2, "Blue");
 
-            var expected = "This Blue vehicle has 2 wheels and has driven 0 miles";
-
-
+            // needed to add a comma and a period to match Sumary
+            var expected = "This Blue vehicle has 2 wheels, and has driven 0 miles.";
+            Assert.Equal(expected, andysBike.Sumary());
         }
-
+        [Fact]
+        public void Drive_Adds5MilesAtATime()
+        {
+            var vehicle1 = new Vehicle(4, "color1");
+            vehicle1.Drive();
+            Assert.Equal(5, vehicle1.MilesDriven);
+            vehicle1.Drive();
+            Assert.Equal(10, vehicle1.MilesDriven);
+            vehicle1.Drive();
+            Assert.Equal(15, vehicle1.MilesDriven);
+        }
+        [Fact]
+        public void Paint_ChangesColor_ToSetNewColor()
+        {
+            var vehicle1 = new Vehicle(4, "color1");
+            vehicle1.Paint("color2");
+            Assert.Equal("color2", vehicle1.Color);
+            vehicle1.Paint("color3");
+            Assert.Equal("color3", vehicle1.Color);
+            vehicle1.Paint("color4");
+            Assert.Equal("color4", vehicle1.Color);
+        }
         // Add more tests here!
     }
 }
